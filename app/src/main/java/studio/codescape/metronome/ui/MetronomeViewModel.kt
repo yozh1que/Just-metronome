adaf0000c59636e45a9fd67f564f8b6868ed2bcc
Dmitriy Filyustin <midnight.88s@gmail.com>
2024-08-12 18:42:42 +0300

    Update MetronomeViewModel to catche beat collection exceptions.

--- app/src/main/java/studio/codescape/metronome/ui/MetronomeViewModel.kt
+++ app/src/main/java/studio/codescape/metronome/ui/MetronomeViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filter
@@ -16,12 +15,14 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import studio.codescape.metronome.R
 import studio.codescape.metronome.domain.model.Metronome
 import studio.codescape.metronome.domain.usecase.GetMetronome
 import studio.codescape.metronome.domain.usecase.GetMetronomeBeat
+import timber.log.Timber
 
 class MetronomeViewModel(
     private val getMetronome: GetMetronome,
@@ -38,18 +39,13 @@ class MetronomeViewModel(
         )
         .filter { it != idleState }
 
-    private val _effects: MutableSharedFlow<Effect> = MutableSharedFlow()
-    val effects: Flow<Effect> = _effects
+    val effects: Flow<Effect> = produceEffects()
+        .shareIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed()
+        )
 
-    init {
-        produceState()
-        collectEffects()
-    }
-
-    private fun produceState() = _commands
-        .receiveAsFlow()
-        .mapNotNull { command -> Unit.takeIf { command == Command.Retry } }
-        .onStart { emit(Unit) }
+    private fun produceState() = initMetronomeDataCollection()
         .flatMapLatest {
             getMetronome()
                 .map<Metronome, State?> { metronome ->
@@ -61,18 +57,26 @@ class MetronomeViewModel(
                         beatsPerMinuteLabel = metronome.beatsPerMinuteLabel
                     )
                 }
-                .catch { emit(null) }
-        }
-
-    private fun collectEffects() {
-        viewModelScope.launch() {
-            getMetronomeBeat()
-                .collect {
-                    println("_effects.emit(Effect.Beat)")
-                    _effects.emit(Effect.Beat)
+                .catch { e ->
+                    Timber.e(e, "Metronome state collection failed.")
+                    emit(null)
                 }
         }
-    }
+
+
+    private fun produceEffects() =
+        initMetronomeDataCollection().flatMapLatest {
+            getMetronomeBeat()
+                .map { Effect.Beat }
+                .catch { e ->
+                    Timber.e(e, "Metronome beat collection failed.")
+                }
+        }
+
+    private fun initMetronomeDataCollection() = _commands
+        .receiveAsFlow()
+        .mapNotNull { command -> Unit.takeIf { command == Command.Retry } }
+        .onStart { emit(Unit) }
 
     fun handleCommand(command: Command) {
         viewModelScope.launch {
