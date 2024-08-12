a9e0e0972466860103c0df924d56d7291a94d2d7
Dmitriy Filyustin <midnight.88s@gmail.com>
2024-08-12 18:41:45 +0300

    Add Timber for console logging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.logging.console)
     testImplementation(libs.junit)
     testImplementation(libs.coroutines.test)
     testImplementation(libs.mockito)
