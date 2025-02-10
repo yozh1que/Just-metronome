763f680ed85ee965bedc3efb02ee69bb658d80e2
Dmitriy Filyustin <midnight.88s@gmail.com>
2025-02-10 23:07:56 +0300

    Update AppComponent to provide coroutine dispatchers.

--- app/src/main/java/studio/codescape/metronome/common/di/CoroutineDispatchers.kt
+++ app/src/main/java/studio/codescape/metronome/common/di/CoroutineDispatchers.kt
@@ -1,11 +1,17 @@
 package studio.codescape.metronome.common.di
 
 import kotlinx.coroutines.CoroutineDispatcher
+import me.tatarka.inject.annotations.Inject
+import studio.codescape.metronome.di.AppScope
 
 typealias MainDispatcher = CoroutineDispatcher
 typealias IoDispatcher = CoroutineDispatcher
+typealias ComputationDispatcher = CoroutineDispatcher
 
+@AppScope
+@Inject
 class CoroutineDispatchers(
     val main: MainDispatcher,
-    val io: IoDispatcher
+    val io: IoDispatcher,
+    val computation: ComputationDispatcher
 )
--- app/src/main/java/studio/codescape/metronome/di/AppComponent.kt
+++ app/src/main/java/studio/codescape/metronome/di/AppComponent.kt
@@ -1,9 +1,14 @@
 package studio.codescape.metronome.di
 
 import android.content.Context
+import kotlinx.coroutines.Dispatchers
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 import me.tatarka.inject.annotations.Scope
+import studio.codescape.metronome.common.di.ComputationDispatcher
+import studio.codescape.metronome.common.di.CoroutineDispatchers
+import studio.codescape.metronome.common.di.IoDispatcher
+import studio.codescape.metronome.common.di.MainDispatcher
 
 @Scope
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER)
@@ -14,5 +19,20 @@ annotation class AppScope
 abstract class AppComponent(
     @get:Provides
     val applicationContext: Context
-)
+) : CoroutineDispatcherComponent
+
+interface CoroutineDispatcherComponent {
+
+
+    val dispatchers: CoroutineDispatchers
+
+    @Provides
+    fun io(): IoDispatcher = Dispatchers.IO
+
+    @Provides
+    fun computation(): ComputationDispatcher = Dispatchers.Default
+
+    @Provides
+    fun main(): MainDispatcher = Dispatchers.Main
+}
 
