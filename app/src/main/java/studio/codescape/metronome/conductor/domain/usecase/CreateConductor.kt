690dc36085e0bbb71ba9c2ac1d2dc99287e9fe11
Dmitriy Filyustin <midnight.88s@gmail.com>
2025-02-11 18:08:48 +0300

    Refactor unused entities.

--- app/src/main/java/studio/codescape/metronome/conductor/domain/usecase/CreateConductor.kt
+++
@@ -1,7 +0,0 @@
-package studio.codescape.metronome.conductor.domain.usecase
-
-import studio.codescape.metronome.conductor.domain.model.Conductor
-
-interface CreateConductor {
-    suspend operator fun invoke(): Conductor?
-}
--- app/src/main/java/studio/codescape/metronome/conductor/domain/usecase/ToggleConductor.kt
+++
@@ -1,9 +0,0 @@
-package studio.codescape.metronome.conductor.domain.usecase
-
-import studio.codescape.metronome.conductor.domain.model.Conductor
-
-class ToggleConductor(
-    private val conductor: Conductor
-) {
-    operator fun invoke() = conductor.handleCommand(Conductor.Command.Toggle)
-}
