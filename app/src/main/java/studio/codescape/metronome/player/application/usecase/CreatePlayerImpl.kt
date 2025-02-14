0c34b53bd19f7c4fa3f9094f01284e06d087f263
Dmitriy Filyustin <midnight.88s@gmail.com>
2025-02-14 23:31:06 +0300

    Refactor CreatePlayer.

--- app/src/main/java/studio/codescape/metronome/player/application/usecase/CreatePlayerImpl.kt
+++
@@ -1,9 +0,0 @@
-package studio.codescape.metronome.player.application.usecase
-
-import studio.codescape.metronome.player.domain.model.Player
-import studio.codescape.metronome.player.domain.usecase.CreatePlayer
-
-class CreatePlayerImpl : CreatePlayer {
-    override suspend fun invoke(): Player? = TODO()
-
-}
--- app/src/main/java/studio/codescape/metronome/player/domain/usecase/CreatePlayer.kt
+++
@@ -1,7 +0,0 @@
-package studio.codescape.metronome.player.domain.usecase
-
-import studio.codescape.metronome.player.domain.model.Player
-
-interface CreatePlayer {
-    suspend fun invoke(): Player?
-}
